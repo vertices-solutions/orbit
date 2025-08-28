e6341221b1acff6b795c03d2655f8c3a2ae7c1a3
Alex <sizykh.a.d@gmail.com>
2025-08-28 10:42:25 -0500

    Ping server

--- hpcd/src/main.rs
+++ hpcd/src/main.rs
@@ -1,4 +1,79 @@
+use anyhow::Context;
+use clap::Parser;
+use proto::agent_server::{Agent, AgentServer};
+use proto::{MfaAnswer, PingReply, PingRequest, StreamEvent};
+use std::net::SocketAddr;
+use std::pin::Pin;
+use std::sync::Arc;
+use tokio_stream::Stream;
+use tonic::Status;
 mod ssh;
-fn main() {
-    println!("hello world!")
+use ssh::{SessionManager, SshParams};
+#[derive(Parser)]
+#[command(version,about,long_about = None)]
+struct Opts {
+    #[arg(short, long)]
+    remote_server: String,
+
+    #[arg(short, long)]
+    username: String,
+
+    #[arg(short, long)]
+    identity_path: String,
+}
+
+type OutStream = Pin<Box<dyn Stream<Item = Result<StreamEvent, Status>> + Send + Sync + 'static>>;
+
+#[derive(Clone)]
+struct AgentSvc {
+    mgr: Arc<SessionManager>,
+}
+#[tonic::async_trait]
+impl Agent for AgentSvc {
+    type LsStream = OutStream;
+    async fn ping(
+        &self,
+        request: tonic::Request<PingRequest>,
+    ) -> Result<tonic::Response<PingReply>, Status> {
+        let req = request.into_inner();
+        match req.message.trim() {
+            "ping" => {
+                return Ok(tonic::Response::new(PingReply {
+                    message: "pong".into(),
+                }));
+            }
+            m => {
+                return Err(Status::invalid_argument(format!(
+                    "expected message 'ping', got '{}'",
+                    m
+                )));
+            }
+        }
+    }
+    async fn ls(
+        &self,
+        request: tonic::Request<tonic::Streaming<MfaAnswer>>,
+    ) -> Result<tonic::Response<Self::LsStream>, Status> {
+    }
+}
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let opts = Opts::parse();
+    let server_addr: SocketAddr = "127.0.0.1:50056".parse()?;
+    let remote_server: SocketAddr = opts
+        .remote_server
+        .parse()
+        .context("failed to parse remote_server address")?;
+
+    let ssh_params = SshParams {
+        addr: remote_server,
+        username: opts.username,
+        identity_path: Some(opts.identity_path),
+        keepalive_secs: 60,
+        ki_submethods: None,
+    };
+    let sm = SessionManager::new(ssh_params);
+
+    println!("hello world!");
+    Ok(())
 }
---
+++ target/debug/deps/hpcd-78523c6c0f9c7c06.d
@@ -0,0 +1,6 @@
+/Users/alexsizykh/Projects/hpc/rust/target/debug/deps/libhpcd-78523c6c0f9c7c06.rmeta: hpcd/src/main.rs hpcd/src/ssh/mod.rs
+
+/Users/alexsizykh/Projects/hpc/rust/target/debug/deps/hpcd-78523c6c0f9c7c06.d: hpcd/src/main.rs hpcd/src/ssh/mod.rs
+
+hpcd/src/main.rs:
+hpcd/src/ssh/mod.rs:
